afa3b8367d443caa1b5751417dcd740ae7a5aae6
Bruno Johnson <brunojs91@gmail.com>
2020-04-27 22:21:38 -0300

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '+5511970321415',
+    category: 'japanese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+5511837268492',
+    category: 'italian'
+  },
+  {
+    name: 'Sapien',
+    address: 'Yuval Noah Harari',
+    phone_number: '+551111111111',
+    category: 'chinese'
+  },
+  {
+    name: 'La Edu',
+    address: 'Rua Almirante Marques de Leão, 638',
+    phone_number: '+298472832',
+    category: 'french'
+  },
+  {
+    name: 'Il bru',
+    address: 'Rua Tupi, 103',
+    phone_number: '+5511982248864',
+    category: 'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
